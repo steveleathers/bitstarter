0a42dde272cab557f39dd2428b563c8f0672dc8d
Ubuntu <ubuntu@ip-10-164-33-47.ec2.internal>
2013-07-07 20:44:02 +0000

    Changed buffer size to 256

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var express = require('express');
-var buf = new Buffer(fs.readFileSync('index.html');
+var buf = new Buffer(256);
+buf.write("index.html");
 var app = express.createServer(express.logger());
 var buffer = buf.toString();
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(fs.readFileSync(buffer));
 });
 
 var port = process.env.PORT || 5000;
