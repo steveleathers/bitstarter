8ba4ed3ab9a350bb8a9a43685754c840874d9e98
Ubuntu <ubuntu@ip-10-164-33-47.ec2.internal>
2013-07-07 19:34:45 +0000

    added readStream

--- web.js
+++ web.js
@@ -5,7 +5,9 @@ buf.write("index.html");
 var app = express.createServer(express.logger());
 var buffer = buf.toString();
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(buffer));
+  var readStream = fs.createReadStream(buffer);
+  readStream.on('data', function(data) {
+    response.send(fs.readFileSync(data));
 });
 
 var port = process.env.PORT || 5000;
