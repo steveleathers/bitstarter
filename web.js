dfdf3e08d182d8a408651b4602cc8e764dd62a78
Ubuntu <ubuntu@ip-10-164-33-47.ec2.internal>
2013-07-07 19:30:34 +0000

    changed web.js buffers

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var express = require('express');
-var buf = new Buffer();
+var buf = new Buffer(64);
 buf.write("index.html");
 var app = express.createServer(express.logger());
-
+var buffer = buf.toString();
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(buf.toString()));
+  response.send(fs.readFileSync(buffer));
 });
 
 var port = process.env.PORT || 5000;
