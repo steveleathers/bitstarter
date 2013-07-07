bbb7973a554acb1d5c00d884d5a6c3b0957af236
Ubuntu <ubuntu@ip-10-164-33-47.ec2.internal>
2013-07-07 19:43:28 +0000

    changed object instantiation

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var express = require('express');
-var buf = new Buffer(64);
+var buf = new Buffer(fs.readFileSync('index.html');
 buf.write("index.html");
 var app = express.createServer(express.logger());
 var buffer = buf.toString();
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(buffer));
+  response.send(fs.readFileSync(buffer);
 });
 
 var port = process.env.PORT || 5000;
